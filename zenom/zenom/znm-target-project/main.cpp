9f3af6ec0fc48bf0eed50c55698384807ec241dd
husnukarakucuk <husnukarakucuk@gmail.com>
2014-06-11 11:59:22 +0000

    - znm-target print usage duzeltmesi

--- zenom/zenom/znm-target-project/main.cpp
+++ zenom/zenom/znm-target-project/main.cpp
@@ -9,28 +9,10 @@ int main(int argc, char *argv[])
 {
     TargetProjectManager targetProjectManager;
 
-    if ( !targetProjectManager.checkParameters(argc, argv))
+    if ( !targetProjectManager.processParameters(argc, argv))
     {
-        targetProjectManager.printUsage();
         exit(1);  /* exit status of the program : non-zero for errors */
     }
-
-
-    /*
-    cppParser.parseFile("/home/zenom/Desktop/zenom/examples/ArduinoDistance/main.cpp");
-
-    std::cout << "Printing Log Vars" << std::endl;
-    for (int i = 0; i < cppParser.logVariables().size(); ++i)
-    {
-        std::cout << "Variable : " << cppParser.logVariables()[i].toStdString() << std::endl;
-    }
-
-    std::cout << std::endl << "Printing Control Vars" << std::endl;
-    for (int i = 0; i < cppParser.controlVariables().size(); ++i)
-    {
-        std::cout << "Variable : " << cppParser.controlVariables()[i].toStdString() << std::endl;
-    }
-    */
 
     return 0;
 }
